bc2e8bb7abbb9cd3e64bd9f130520b5da579bbb9
Salmann Mogalia <smogalia@hotmail.fr>
2023-01-13 07:18:05 +0400

    add newRound and player_turn

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 let dice = document.getElementById("dice");
 let roll = document.getElementById("role_dice");
 let count = 0;
+let result1 = document.getElementById("result1");
+let result2 = document.getElementById("result2");
+let score = document.getElementById("score");
+let player1_selected = document.getElementById("player1_selected");
+let player2_selected = document.getElementById("player2_selected");
+let reset = document.getElementById("reset");
+
+let player_turn= Math.floor(Math.random() * 2) + 1;
+
+//nouvelle partie
+reset.addEventListener("click", function () {
+    newRound();
+});
+
+//indique le joueur qui doit jouer
+function newRound(){
+    if(player_turn === 1){
+        player_turn = 2;
+        player2_selected.style.opacity = "1";
+        player1_selected.style.opacity = "0";
+    } else {
+        player_turn = 1;
+        player1_selected.style.opacity = "1";
+        player2_selected.style.opacity = "0";
+    }
+}
+
+
+
 
 roll.addEventListener("click", function () {
     DiceAnimation();
+    count = count + a;
+    //enregistre le score du joueur en fonction de son tour
+    if (player_turn === 1) {
+        result1.innerText = count;
+    } else {
+        result2.innerText = count;
+    }
 });
 
 function rollDice() {
@@ -16,8 +52,6 @@ function DiceAnimation() {
             a=rollDice();
             dice.setAttribute('src', './assets/img/face-' + a + '.png');
             score.innerText = a;
-        }, 100 * i);
+        }, 50 * i);
     }
-    count=count+a;
-    winner.innerText = count;
 }
